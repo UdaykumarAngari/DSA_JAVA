b149586bc1e0d8d29781b9fab03486315e917da3
UdaykumarAngari <140047183+UdaykumarAngari@users.noreply.github.com>
2026-03-23 16:28:15 +0530

    Time: 0 ms (100%), Space: 45.4 MB (0%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+
+
+        // int oddCount = 0 ;
+        // int n = nums1.length;
+
+        // for(int num : nums1){
+        //     if(Math.abs(num)%2 != 0){
+        //         oddCount++;
+        //     }
+        // }
+
+        // boolean canBeAllEven = (oddCount== 0 || oddCount >=2);
+
+        // Boolean canBeAllOdd = (oddCount >0);
+
+        // return canBeAllEven || canBeAllOdd;
+    }
+}
