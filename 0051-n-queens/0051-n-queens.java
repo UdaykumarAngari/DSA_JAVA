d289fb488a4bcfc439ecf0325152593393220732
Angari Udaykumar <140047183+UdaykumarAngari@users.noreply.github.com>
2026-06-04 17:55:32 +0530

    Time: 1 ms (99.93%), Space: 46.8 MB (27.73%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,56 +1,109 @@
 import java.util.*;
 
 class Solution {
-    public boolean isSafe(int row, int col, char[][] board, int n) {
+    // public boolean isSafe(int row, int col, char[][] board, int n) {
          
-        for (int j = 0; j < col; j++) {
-            if (board[row][j] == 'Q') return false;
-        }
+    //     for (int j = 0; j < col; j++) {
+    //         if (board[row][j] == 'Q') return false;
+    //     }
         
-        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
-            if (board[i][j] == 'Q') return false;
-        }
+    //     for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+    //         if (board[i][j] == 'Q') return false;
+    //     }
  
-        for (int i = row, j = col; i < n && j >= 0; i++, j--) {
-            if (board[i][j] == 'Q') return false;
-        }
+    //     for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+    //         if (board[i][j] == 'Q') return false;
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
-    public void solve(int col, char[][] board,
-                      List<List<String>> ans, int n) {
+    // public void solve(int col, char[][] board,
+    //                   List<List<String>> ans, int n) {
+    //     if (col == n) {
+    //         List<String> temp = new ArrayList<>();
+    //         for (int i = 0; i < n; i++) {
+    //             temp.add(new String(board[i]));
+    //         }
+    //         ans.add(temp);
+    //         return;
+    //     }
+ 
+    //     for (int row = 0; row < n; row++) {
+    //         if (isSafe(row, col, board, n)) {
+
+    //             board[row][col] = 'Q';    
+
+    //             solve(col + 1, board, ans, n); 
+
+    //             board[row][col] = '.';         
+    //         }
+    //     }
+    // }
+
+    // public List<List<String>> solveNQueens(int n) {
+    //     List<List<String>> ans = new ArrayList<>();
+    //     char[][] board = new char[n][n];
+
+    //     for (int i = 0; i < n; i++) {
+    //         Arrays.fill(board[i], '.');
+    //     }
+
+    //     solve(0, board, ans, n);
+    //     return ans;
+    // }
+
+   
+    public void solve(int col, char[][] board, int n,
+                      int[] leftRow, int[] upperDiagonal, int[] lowerDiagonal,
+                      List<List<String>> res) {
+
         if (col == n) {
             List<String> temp = new ArrayList<>();
             for (int i = 0; i < n; i++) {
                 temp.add(new String(board[i]));
             }
-            ans.add(temp);
+            res.add(temp);
             return;
         }
- 
+
+         
         for (int row = 0; row < n; row++) {
-            if (isSafe(row, col, board, n)) {
+           
+            if (leftRow[row] == 0 && lowerDiagonal[row + col] == 0 &&
+                upperDiagonal[n - 1 + col - row] == 0) {
 
-                board[row][col] = 'Q';    
+            
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row + col] = 1;
+                upperDiagonal[n - 1 + col - row] = 1;
 
-                solve(col + 1, board, ans, n); 
+                
+                solve(col + 1, board, n, leftRow, upperDiagonal, lowerDiagonal, res);
 
-                board[row][col] = '.';         
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row + col] = 0;
+                upperDiagonal[n - 1 + col - row] = 0;
             }
         }
     }
-    
+
+  
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans = new ArrayList<>();
+        List<List<String>> res = new ArrayList<>();
         char[][] board = new char[n][n];
+        
+        for (char[] row : board) Arrays.fill(row, '.');
 
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(board[i], '.');
-        }
+        int[] leftRow = new int[n];
+        int[] lowerDiagonal = new int[2 * n - 1];
+        int[] upperDiagonal = new int[2 * n - 1];
 
-        solve(0, board, ans, n);
-        return ans;
+        solve(0, board, n, leftRow, upperDiagonal, lowerDiagonal, res);
+        return res;
     }
+
 }
  
