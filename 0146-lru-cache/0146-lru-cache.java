516853f3cd9a18f84bc90f881c54074bff1418ae
Angari Udaykumar <140047183+UdaykumarAngari@users.noreply.github.com>
2026-06-18 11:48:13 +0530

    Time: 47 ms (52.18%), Space: 132.4 MB (28.78%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,77 +1,101 @@
 class LRUCache {
+
     class Node {
         int key;
-        int val ;
-        Node prev; 
+        int val;
+        Node prev;
         Node next;
 
-        public Node(int key, int val){
+        Node(int key, int val) {
             this.key = key;
             this.val = val;
         }
     }
 
+    // Dummy head and tail
+    // head side = most recently used
+    // tail side = least recently used
     Node head = new Node(-1, -1);
     Node tail = new Node(-1, -1);
 
     int cap;
+
+    // key -> Node
     HashMap<Integer, Node> map = new HashMap<>();
-    
+
     public LRUCache(int capacity) {
         cap = capacity;
+
+        // Initially list is empty:
+        // head <-> tail
         head.next = tail;
         tail.prev = head;
     }
-    void addNode(Node newNode){
+
+    // Add node right after head
+    // This makes it most recently used
+    void addNode(Node newNode) {
         Node temp = head.next;
+
         newNode.next = temp;
         newNode.prev = head;
-        
+
         head.next = newNode;
         temp.prev = newNode;
     }
 
-    void deleteNode(Node delNode){
-        Node delPrev = delNode.prev;
-        Node delNext = delNode.next;
+    // Delete a node from its current position
+    void deleteNode(Node delNode) {
+        Node prevNode = delNode.prev;
+        Node nextNode = delNode.next;
 
-        delPrev.next = delNext ;
-        delNext.prev = delPrev;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
     }
 
-
     public int get(int key) {
-        if(map.containsKey(key)){
+
+        if(map.containsKey(key)) {
+
+            // map.get(key) returns the Node
             Node resNode = map.get(key);
-            int res = resNode.val;
-            map.remove(key);
+
+            int value = resNode.val;
+
+            // Since it is accessed now,
+            // move it to most recently used position
             deleteNode(resNode);
             addNode(resNode);
 
-            map.put(key, head.next);
-            return res;
+            return value;
         }
+
         return -1;
     }
-    
+
     public void put(int key, int value) {
-        if (map.containsKey(key)) {
+
+        // If key already exists,
+        // remove old node first
+        if(map.containsKey(key)) {
             Node existingNode = map.get(key);
-            map.remove(key);
             deleteNode(existingNode);
+            map.remove(key);
         }
-        if (map.size() == cap) {
-            map.remove(tail.prev.key);
-            deleteNode(tail.prev);
+
+        // If cache is full,
+        // remove least recently used node
+        if(map.size() == cap) {
+            Node lru = tail.prev;
+
+            deleteNode(lru);
+            map.remove(lru.key);
         }
-        addNode(new Node(key, value));
-        map.put(key, head.next);
+
+        // Insert new node as most recently used
+        Node newNode = new Node(key, value);
+
+        addNode(newNode);
+        map.put(key, newNode);
     }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
